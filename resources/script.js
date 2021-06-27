3648f132a456c1286270207657e425d325b24f75
mleesers <mduron432@gmail.com>
2021-06-26 21:16:03 -0500

    finished basic javascript entities

--- resources/script.js
+++ resources/script.js
@@ -1,33 +1,88 @@
 correct = 0;
 wrong = 0;
+total = wrong + correct;
 highScore = 0;
-secondsLeft = 60;
 function timer(){
+    secondsLeft = 60;
     var timeInterval = setInterval(function(){
-        console.log(secondsLeft);
         secondsLeft--;
     },1000);
-}
-function correct(){
-
-}
-function wrong(){
-    
-}
-function main(){
-    console.log("start Button")
-    timer();
-    question1();
-}
-
-function question1(){
-    window.alert("Commonly used data types DO NOT Include: \n 1.strings, 2.booleans 3.alerts 4.numbers");
-    answer = window.prompt("Enter a number bewteen 1-4: ");
-    if(answer === 3){
-        window.alert("Correct!");
-        correct += 1;
-    }else{
-        window.alert("Incorrect!");
-        wrong += 1;
+    console.log(secondsLeft);
+    if(secondsLeft != 0 || total != 5){
+        main();
+    }
+    else if(secondsLeft == 0);{
+    console.log("You have failed to answer all the questions in time")
     }
 }
+timer();
+function correctAnswer(){
+    window.alert("Correct!");
+        correct += 1;
+}
+function wrongAnswer(){
+    window.alert("Incorrect!");
+        wrong += 1;
+        secondsLeft -= 10;
+}
+function main(){
+    console.log("start Button");
+    question1();
+    console.log(correct);
+    console.log(wrong);
+    highScore = correct;
+    console.log(highScore)
+}
+
+
+function question1(){
+    window.alert("Commonly used data types DO NOT Include ____. \n 1.strings, 2.booleans 3.alerts 4.numbers");
+    answer = window.prompt("Enter a number bewteen 1-4: ");
+    if(answer == 3){
+        correctAnswer();
+    }else{
+        wrongAnswer();
+    }
+    question2();
+}
+function question2(){
+    window.alert("The condition in an if/else statement is enclosed within _____. \n 1.quotes, 2.curly brackets 3.parentheses 4.square brackets");
+    answer = window.prompt("Enter a number bewteen 1-4: ");
+    if(answer == 2){
+        correctAnswer();
+    }else{
+        wrongAnswer();
+    }
+    question3();
+}
+function question3(){
+    window.alert("Arrays in JavaScript can be used to store ____. \n 1.numbers and strings 2.other arrays 3.booleans 4.all of the above");
+    answer = window.prompt("Enter a number bewteen 1-4: ");
+    if(answer == 4){
+        correctAnswer();
+    }else{
+        wrongAnswer();
+    }
+    question4();
+}
+function question4(){
+    window.alert("string values must be enclose within ____ when being assigned to variables. \n 1.commas, 2.curly brackets 3.quotes 4.parentheses");
+    answer = window.prompt("Enter a number bewteen 1-4: ");
+    if(answer == 3){
+        correctAnswer();
+    }else{
+        wrongAnswer();
+    }
+    question5();
+}
+
+function question5(){
+    window.alert("A very useful tool used during development and debugging for printing content to the debugger is: \n 1.JavaScript, 2.terminal/bash 3.for loops 4.console.log");
+    answer = window.prompt("Enter a number bewteen 1-4: ");
+    if(answer == 4){
+        correctAnswer();
+    }else{
+        wrongAnswer();
+    }
+}
+
