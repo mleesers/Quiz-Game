586b79427230ad055061ad140e354f529690b33e
mleesers <mduron432@gmail.com>
2021-06-26 20:33:12 -0500

    added some functions and completed question1

--- resources/script.js
+++ resources/script.js
@@ -1,7 +1,33 @@
+correct = 0;
+wrong = 0;
+highScore = 0;
+secondsLeft = 60;
 function timer(){
-    secondsLeft = 30;
     var timeInterval = setInterval(function(){
         console.log(secondsLeft);
         secondsLeft--;
     },1000);
+}
+function correct(){
+
+}
+function wrong(){
+    
+}
+function main(){
+    console.log("start Button")
+    timer();
+    question1();
+}
+
+function question1(){
+    window.alert("Commonly used data types DO NOT Include: \n 1.strings, 2.booleans 3.alerts 4.numbers");
+    answer = window.prompt("Enter a number bewteen 1-4: ");
+    if(answer === 3){
+        window.alert("Correct!");
+        correct += 1;
+    }else{
+        window.alert("Incorrect!");
+        wrong += 1;
+    }
 }
