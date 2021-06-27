67a1d006f0ddf2cac3fcf64929338b0538cff6d0
mleesers <mduron432@gmail.com>
2021-06-27 01:23:06 -0500

    added click functionality to question1

--- resources/script.js
+++ resources/script.js
@@ -1,75 +1,90 @@
+answer1 = document.getElementById("answer1");
+answer2 = document.getElementById("answer2");
+answer3 = document.getElementById("answer3");
+answer4 = document.getElementById("answer4");
+next = document.getElementById("next");
+
 correct = 0;
 wrong = 0;
 total = wrong + correct;
 highScore = 0;
+
+main();
+
+
 function timer(){
     secondsLeft = 60;
-    var timeInterval = setInterval(function(){
+    timeInterval = setInterval(function(){
+        console.log(secondsLeft);
         secondsLeft--;
     },1000);
-    console.log(secondsLeft);
-    if(secondsLeft != 0 || total != 5){
-        main();
-    }
-    else if(secondsLeft == 0);{
-    console.log("You have failed to answer all the questions in time")
-    }
+    return secondsLeft;
 }
-timer();
+
 function correctAnswer(){
-    window.alert("Correct!");
-        correct += 1;
+    window.alert("fine")
+    correct += 1;
+    n = true;
 }
 function wrongAnswer(){
-    window.alert("Incorrect!");
-        wrong += 1;
-        secondsLeft -= 10;
+    window.alert("not today")
+    wrong += 1;
+    secondsLeft -= 10;
+    n = true;
 }
 function main(){
-    console.log("start Button");
+    
     question1();
-    /*console.log(correct);
-    console.log(wrong);
-    highScore = correct;
-    console.log(highScore)*/
+    timer();
 }
 main();
 
 function question1(){
-   document.getElementById("question").innerHTML = "Commonly used data types DO NOT Include ____";
+   document.getElementById("question").innerHTML = "Commonly used data types DO NOT Include ____.";
     for(i=1;i<=4;i++){
-        arr = ["empty","strings","booleans","alerts","numbers"]
+        arr = ["empty","strings","booleans","alerts","numbers"];
+        document.getElementById("answer" + i).innerHTML = arr[i];
+    }
+    answer1.addEventListener("click", function() {
+        wrongAnswer();
+        question2();
+    });
+    answer2.addEventListener("click", function() {
+        wrongAnswer();
+        question2();
+    });
+    answer3.addEventListener("click", function() {
+        correctAnswer();
+        question2();
+    });
+    answer4.addEventListener("click", function() {
+        wrongAnswer();
+        question2();
+    });
+}
+function question2(){
+    document.getElementById("question").innerHTML = "The condition in an if/else statement is enclosed within _____.";
+    for(i=1;i<=4;i++){
+        arr = ["empty","quotes","curly brackets","parentheses","square brackets"]
         document.getElementById("answer" + i).innerHTML = arr[i]
     }
     document.getElementById("answer1").addEventListener("click", wrongAnswer);
-    document.getElementById("answer2").addEventListener("click", wrongAnswer);
-    document.getElementById("answer3").addEventListener("click", correctAnswer);
+    document.getElementById("answer2").addEventListener("click", correctAnswer);
+    document.getElementById("answer3").addEventListener("click", wrongAnswer);
     document.getElementById("answer4").addEventListener("click", wrongAnswer);
-
-
-
-    //question2();
-}/*
-function question2(){
-    window.alert("The condition in an if/else statement is enclosed within _____. \n 1.quotes, 2.curly brackets 3.parentheses 4.square brackets");
-    answer = window.prompt("Enter a number bewteen 1-4: ");
-    if(answer == 2){
-        correctAnswer();
-    }else{
-        wrongAnswer();
-    }
-    question3();
+    document.getElementById("next").addEventListener("click", question3)
+    
+    
 }
+/*
 function question3(){
-    window.alert("Arrays in JavaScript can be used to store ____. \n 1.numbers and strings 2.other arrays 3.booleans 4.all of the above");
-    answer = window.prompt("Enter a number bewteen 1-4: ");
-    if(answer == 4){
-        correctAnswer();
-    }else{
-        wrongAnswer();
+    document.getElementById("question").innerHTML = "Arrays in JavaScript can be used to store ____.";
+    for(i=1;i<=4;i++){
+        arr = ["empty","numbers and strings","other arrays","booleans","all of the above"]
+        document.getElementById("answer" + i).innerHTML = arr[i]
     }
-    question4();
 }
+
 function question4(){
     window.alert("string values must be enclose within ____ when being assigned to variables. \n 1.commas, 2.curly brackets 3.quotes 4.parentheses");
     answer = window.prompt("Enter a number bewteen 1-4: ");
@@ -89,6 +104,4 @@ function question5(){
     }else{
         wrongAnswer();
     }
-}
-
-*/
+}*/
